e4479b1e84906488b0d38746e97ac6f7e56be01d
lfdversluis <laurens-versluis@hotmail.com>
2014-05-14 13:09:26 +0200

    changes stuff

--- anontunnel/main.py
+++ anontunnel/main.py
@@ -9,8 +9,6 @@ import os
 #adjust the PYTHON_EGG_CACHE
 os.environ["PYTHON_EGG_CACHE"] = "/data/data/com.AT3.anontunnel/cache"
 
-from anontunnel.atunnel import AnonTunnel
-
 """
 AnonTunnel CLI interface
 """
@@ -20,45 +18,35 @@ print os.getcwd()
 print '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 
-from anontunnel.community import ProxyCommunity, \
-    ProxySettings
-
 # set the background color to white
 from kivy.core.window import Window
 Window.clearcolor = (1, 1, 1, 1)
 
 class IORedirector(object): # A general class for redirecting I/O to this Text widget.
-	def __init__(self,log_textview):
-		self.log_textview = log_textview
+    def __init__(self,log_textview):
+        self.log_textview = log_textview
 
 class StdoutRedirector(IORedirector): # A class for redirecting stdout to this Text widget.
-	def write(self,str):
-		self.log_textview.text = self.log_textview.text+ str
+    def write(self,str):
+        self.log_textview.text = self.log_textview.text+ str
 
 class AnonTunnelScreen(BoxLayout):
-	def __init__(self, **kwargs):
-		super(AnonTunnelScreen, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(AnonTunnelScreen, self).__init__(**kwargs)
 
-		# redirect the stdout to the log_textview
-		import sys
-		sys.stdout = StdoutRedirector(self.log_textview)
-
-	def startAnonTunnel(self):
-		print 'Starting anon tunnel'
-		
-		socks5_port = None
-		proxy_settings = ProxySettings()
-		crawl = False
-
-		self.anon_tunnel = AnonTunnel(socks5_port, proxy_settings, crawl)
-		self.anon_tunnel.start()
-
-	def stopAnonTunnel(self):
-		print 'Stopping anon tunnel'
-		self.anon_tunnel.stop();
+        # redirect the stdout to the log_textview
+        import sys
+        sys.stdout = StdoutRedirector(self.log_textview)
 
 class AnonTunnelApp(App):
     def build(self):
+
+        if platform == 'android':
+            from android import AndroidService
+            service = AndroidService('Anon Tunnel Service', 'Runs the anontunnels in the background.')
+            service.start('AnonTunnel Service started')
+            self.service = service
+
         return AnonTunnelScreen()
 
 if __name__ == '__main__':

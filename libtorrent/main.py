b109f7056cc8ebe2d00bca3d4cb4281b43d6b688
Rolf Jagerman <rjagerman@gmail.com>
2014-05-21 18:27:51 +0200

    Added libtorrent app for testing libtorrent.
    It opens a hard coded path to the torrent file, be sure to modify this in main.py if you want to test it.

---
+++ libtorrent/main.py
@@ -0,0 +1,23 @@
+import libtorrent as lt
+import time
+import sys
+
+ses = lt.session()
+ses.listen_on(6881, 6891)
+
+info = lt.torrent_info("/storage/extSdCard/Torrents/ubuntu-14.04-desktop-i386.iso.torrent")
+h = ses.add_torrent({'ti': info, 'save_path': '/storage/extSdCard/Torrents/'})
+print 'starting', h.name()
+
+while (not h.is_seed()):
+   s = h.status()
+
+   state_str = ['queued', 'checking', 'downloading metadata', \
+      'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+   print('%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+      (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+      s.num_peers, state_str[s.state]))
+
+   time.sleep(1)
+
+print h.name(), 'complete'

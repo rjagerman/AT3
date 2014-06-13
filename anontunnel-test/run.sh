346fabdad3296865ea1960cffe8f0bb98b3866ab
Martijn de Vos <mdmartijndevos@gmail.com>
2014-06-13 15:20:30 +0200

    added logger to the run script

--- anontunnel-test/run.sh
+++ anontunnel-test/run.sh
@@ -4,6 +4,7 @@ RUNS=500
 APKLOCATION=/home/martijn/Documenten/python-for-android/dist/Test/bin/anontunneltest-1.0-debug.apk
 PACKAGENAME=org.tribler.at3.anontunneltest
 ACTIVITYNAME=org.renpy.android.PythonActivity
+PIDCATLOCATION=/home/martijn/Documenten/AT3/pidcat/pidcat.py
 
 for i in $(seq 2 $RUNS)
 do
@@ -12,14 +13,18 @@ do
 	adb install $APKLOCATION
 	adb shell am start -n $PACKAGENAME/$ACTIVITYNAME
 
+	# start pidcat
+	FILENAME=`date +"%H%M%m%d%Y"`
+	python $PIDCATLOCATION $PACKAGENAME > "$FILENAME.log"
+
 	# check if the application still exists
 	while :
 	do
 		OUT=`adb shell ps | grep org.tribler.at3.anontunneltest | awk '{print $9}'`
 		if [ "X$OUT" == "X" ]; then
+			pkill -f pidcat
 			break
 		fi
 		sleep 5
 	done
-
 done
